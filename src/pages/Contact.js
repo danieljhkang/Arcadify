308b7768ca006ab793bdbea5c77b6ab916d55a06
Daniel Kang <dkang.jh@gmail.com>
2023-07-24 14:57:09 +0900

    updated contact

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -9,7 +9,7 @@ const Contact = () => {
         <p className="about-text">
             If you find any bugs, would like to make
             any comments/suggestions, or have any other inquiries, contact me
-            at <a className="contact-link" href="/contact">dkang.jh@gmail.com</a> or reach out to my&nbsp;
+            at <a className="contact-link" href="/contact">arcadifyapp@gmail.com</a> or reach out to my&nbsp;
             <a className="contact-link" href="https://www.linkedin.com/in/daniel-kang-46b1a51b9/">linkedIn!</a>
         </p>
        
